5efa22e807939647c4b2e26efade9e6f4aae2ac4
Markus Grüneis <gimpf@gimpf.org>
2014-07-26 20:06:31 +0200

    use directory ./data/ instead of the point-wasteland from before
    this happens when you always had everything compiled in, just hack
    up a configuration file before commiting, and never executing that
    ... thing outside the debugger: on first try on the console
    everything just throws exceptions.
    
    now, exceptions are fine, once in a while, but this wasn't.
    
    hopefully the last the last change ever

--- Gimpf.Skyirm.AlchemyCalculator/Program.fs
+++ Gimpf.Skyirm.AlchemyCalculator/Program.fs
@@ -9,10 +9,10 @@ let time f =
     result
 
 let convertWikiImportedData () =
-    let effects = Importer.Effects.readAll <| fromFile @"..\..\..\data\Skyrim Effects.txt"
-    FileIO.writeEffects (toFile @"..\..\..\data\alchemy-effects.csv") effects
-    let ingredients = Importer.Ingredients.readAll <| fromFile @"..\..\..\data\Skyrim Ingredients.txt"
-    FileIO.writeIngredients (toFile @"..\..\..\data\alchemy-ingredients.csv") ingredients
+    let effects = Importer.Effects.readAll <| fromFile @"data\Skyrim Effects.txt"
+    FileIO.writeEffects (toFile @"data\alchemy-effects.csv") effects
+    let ingredients = Importer.Ingredients.readAll <| fromFile @"data\Skyrim Ingredients.txt"
+    FileIO.writeIngredients (toFile @"data\alchemy-ingredients.csv") ingredients
 
 let formatRecipe (Recipe(effect, ingredients)) =
     let ingredientList =
@@ -24,12 +24,12 @@ let formatRecipe (Recipe(effect, ingredients)) =
     sprintf "%s => %s" ingredientList effect
 
 let showPreferedRecipes () =
-    let effects      = FileIO.readEffects     (fromFile @"..\..\..\data\alchemy-effects.csv")
-    let ingredients  = FileIO.readIngredients (fromFile @"..\..\..\data\alchemy-ingredients.csv")
+    let effects      = FileIO.readEffects     (fromFile @"data\alchemy-effects.csv")
+    let ingredients  = FileIO.readIngredients (fromFile @"data\alchemy-ingredients.csv")
     let drugs        = Drugs.createDescription effects ingredients
-    let available    = FileIO.readStoreItems  (fromFile @"..\..\..\data\available-ingredients.csv")
+    let available    = FileIO.readStoreItems  (fromFile @"data\available-ingredients.csv")
                        |> (toStore drugs)
-    let knownRecipes = FileIO.readRecipeIngredientsList (fromFile @"..\..\..\data\known-recipes.csv")
+    let knownRecipes = FileIO.readRecipeIngredientsList (fromFile @"data\known-recipes.csv")
                        |> List.choose (Recipes.makeRecipeFromNotes drugs)
     let brewery = knownRecipes |> learnAll { Store = available ; Known = Map.empty }
 
@@ -39,7 +39,7 @@ let showPreferedRecipes () =
     //     10.00, avoidInpure
     //     7.00 , preferEffect "Restore Health"
     //     7.00 , preferEffect "Restore Magicka" ]
-    let preferredDrugs = FileIO.readPreferences (fromFile @"..\..\..\data\preferences.csv")
+    let preferredDrugs = FileIO.readPreferences (fromFile @"data\preferences.csv")
 
     let knownRecipeUpdate =
         fun () -> generateRecipes drugs brewery preferredDrugs
@@ -53,7 +53,7 @@ let showPreferedRecipes () =
     let knownRecipes = List.concat [ knownRecipes ; knownRecipeUpdate ]
                        |> Seq.distinct
 
-    FileIO.writeRecipeIngredientsList (toFile @"..\..\..\data\known-recipes-new.csv") knownRecipes
+    FileIO.writeRecipeIngredientsList (toFile @"data\known-recipes-new.csv") knownRecipes
 
 [<EntryPoint>]
 let main argv =
